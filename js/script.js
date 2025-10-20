19708c46b19333afc5af87042fb0355c17d6ddd1
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-10-21 00:32:57 +0530

    feat: add myLibrary array and Book constructor

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// Array to store all books in the library
+const myLibrary = [];
+
+// Constructor function to create a Book object
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
