db23fb5dc8b4d2a9aee75f8730d8fde78eb5b90d
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-10-21 00:41:37 +0530

    feat: implement addBookToLibrary function

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
 }
+
+// Function to prompt user for book details and add it to the library
+function addBookToLibrary() {
+    const title = prompt("Title: ");
+    const author = prompt("Author: ");
+    const pages = prompt("Total Pages: ");
+    
+    myLibrary.push(new Book(title, author, pages)); // Create book and add to library
+}
+
