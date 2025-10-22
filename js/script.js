e8734440acdcc7b6e8255890579c3a5494b638bb
nishadnp <69951135+nishadnp@users.noreply.github.com>
2025-10-22 13:31:12 +0530

    feat: add renderLibrary to display all the books

--- js/script.js
+++ js/script.js
@@ -18,7 +18,36 @@ function addBookToLibrary() {
     myLibrary.push(new Book(title, author, pages)); // Create book and add to library
 }
 
+// Function to render all the books in library on the page
+function renderLibrary() {
+    for (let book of myLibrary) {
+
+        // Create a container element for the book
+        const theBook = document.createElement('article');
+
+        // Create elements to display the book details
+        const bookTitle = document.createElement('p');
+        const bookAuthor = document.createElement('p');
+        const bookPages = document.createElement('p');
+
+        // Set text content for each element
+        bookTitle.textContent = `Title: ${book.title}`;
+        bookAuthor.textContent = `Author: ${book.author}`;
+        bookPages.textContent = `Pages: ${book.pages}`;
+
+        // Append the details to the book container
+        theBook.appendChild(bookTitle);
+        theBook.appendChild(bookAuthor);
+        theBook.appendChild(bookPages);
+
+        // Append the book container to the body
+        document.body.appendChild(theBook);
+    }
+}
+
 // Example calls to test the functionality
 addBookToLibrary();
 addBookToLibrary();
 addBookToLibrary();
+
+renderLibrary();
